a20d91e6722e70142819a024189465aba08016ae
riverkid59 <70954351+riverkid59@users.noreply.github.com>
2020-12-01 10:05:09 +0100

    quete terminée

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,28 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
+    public Doctor doctor(@PathVariable int number, Doctor doctor) {
+    	if (number == 13) {
+    		Doctor jekill = new Doctor(13, "Jodie Whittaker");
+    				return jekill;
+    	} else if (1 < number && number < 14) {
+    		throw new ResponseStatusException(HttpStatus.SEE_OTHER, "Voir un autre docteur ");
+    	} else if (number == 42) {
+    		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
+    	}
         return null;
     }
 }
